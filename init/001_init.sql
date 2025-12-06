e0ff371708484c070e4b042a7fd3515a7f241d70
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2025-12-06 17:08:23 +0900

    miss

--- init/001_init.sql
+++ init/001_init.sql
@@ -155,7 +155,12 @@ CREATE TABLE
   );
 
 CREATE TABLE
-  `discords` (`id` INT PRIMARY KEY AUTO_INCREMENT, `discord_id` VARCHAR(255) UNIQUE NOT NULL, `user_id` VARCHAR(255) NOT NULL);
+  `discords` (
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
+    `custom_id` VARCHAR(255) UNIQUE NOT NULL,
+    `discord_id` VARCHAR(255) UNIQUE NOT NULL,
+    `user_id` VARCHAR(255) NOT NULL
+  );
 
 CREATE TABLE
   `user_role` (`id` INT PRIMARY KEY AUTO_INCREMENT, `user_id` VARCHAR(255) NOT NULL, `role_id` VARCHAR(255) NOT NULL);
