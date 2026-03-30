7aca59a88d9acc57298e3adf9c54fdd9c1e8867f
Yuito Akatsuki (Tani Yutaka) <yuito@yuito-it.jp>
2026-03-31 01:43:23 +0900

    DATABASE_URLのエクスポートを削除

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,5 @@ latest_tag=$(git describe --tags --abbrev=0)
 git checkout "$latest_tag"
 
 # 3. migration up
-export DATABASE_URL="mysql://user:pass@tcp(host:3306)/dbname?multiStatements=true"
 export MIGRATIONS_DIR="./migrations"
 migrate -path "$MIGRATIONS_DIR" -database "$DATABASE_URL" up
